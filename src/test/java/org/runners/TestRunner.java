011b7bfac686e1f92faf85a4b12b63ed8486633b
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-04-01 22:13:31 +0200

    Fix ci

--- src/test/java/org/runners/TestRunner.java
+++ src/test/java/org/runners/TestRunner.java
@@ -6,10 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "org.stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
-        monochrome = true
+        features = "src/test/resources/features", // Chemin des fichiers .feature
+        glue = "org.stepdefinitions", // Package contenant les Step Definitions
+        plugin = {
+                "pretty", // Pour un affichage plus lisible dans la console
+                "html:target/cucumber-reports/cucumber.html", // Génère un rapport HTML
+                "json:target/cucumber-reports/cucumber.json", // Génère un rapport JSON pour l'intégration avec d'autres outils
+                "io.qameta.allure.cucumber7jvm.AllureReporter" // Intégration avec Allure pour la génération de rapports visuels
+        },
+        monochrome = true // Rend la sortie console plus lisible
 )
 public class TestRunner {
 }
